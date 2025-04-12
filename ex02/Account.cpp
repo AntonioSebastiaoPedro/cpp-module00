eaef0522fdbc2cab856849fd345694a032f2c1aa
António Sebastião Pedro <antoniosebastiaopedro19@outlook.com>
2025-04-12 15:38:28 +0100

    Adiciona timestamp às operações da conta
    Implementa registro de timestamp para criação e encerramento de contas, depósitos e saques, fornecendo um histórico cronológico das transações.

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,12 +6,13 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 11:07:13 by ansebast          #+#    #+#             */
-/*   Updated: 2025/04/12 14:38:07 by ansebast         ###   ########.fr       */
+/*   Updated: 2025/04/12 15:36:13 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "Account.hpp"
 # include <iostream>
+# include <ctime>
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
@@ -40,6 +41,7 @@ int	Account::getNbWithdrawals( void )
 
 void	Account::displayAccountsInfos( void )
 {
+	Account::_displayTimestamp();
 	std::cout << "accounts:" << getNbAccounts()
 		  << ";total:" << getTotalAmount()
 		  << ";deposits:" << getNbDeposits()
@@ -55,7 +57,9 @@ Account::Account( int initial_deposit )
 	this->_nbDeposits = 0;
 	this->_nbWithdrawals = 0;
 	_totalAmount += initial_deposit;
+	
 	_nbAccounts++;
+	Account::_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";"
 		  << "amount:" << initial_deposit << ";"
 		  << "created"
@@ -64,6 +68,7 @@ Account::Account( int initial_deposit )
 
 Account::~Account( void )
 {
+	Account::_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";"
 		  << "amount:" << this->_amount << ";"
 		  << "closed"
@@ -76,7 +81,9 @@ void	Account::makeDeposit( int deposit )
 	this->_nbDeposits++;
 	
 	_totalAmount += deposit;
+	
 	_totalNbDeposits++;
+	Account::_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";"
 		  << "p_amount:" << this->_amount - deposit << ";"
 		  << "deposit:" << deposit << ";"
@@ -89,6 +96,7 @@ bool	Account::makeWithdrawal( int withdrawal )
 {
 	if (checkAmount() - withdrawal < 0)
 	{
+		Account::_displayTimestamp();
 		std::cout << "index:" << this->_accountIndex << ";"
 			<< "p_amount:" << this->_amount << ";"
 			<< "withdrawal:refused"
@@ -99,7 +107,9 @@ bool	Account::makeWithdrawal( int withdrawal )
 	this->_amount -= withdrawal;
 	this->_nbWithdrawals++;
 	_totalAmount -= withdrawal;
+	
 	_totalNbWithdrawals++;
+	Account::_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";"
 		  << "p_amount:" << this->_amount + withdrawal << ";"
 		  << "withdrawal:" << withdrawal << ";"
@@ -116,6 +126,7 @@ int	Account::checkAmount( void ) const
 
 void	Account::displayStatus( void ) const
 {
+	Account::_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";"
 		<< "amount:" << this->_amount << ";"
 		<< "deposits:" << this->_nbDeposits << ";"
@@ -125,5 +136,21 @@ void	Account::displayStatus( void ) const
 
 void	Account::_displayTimestamp( void )
 {
+	std::time_t now = time(NULL);
+	std::tm* timestamp = std::localtime(&now);
 
+	std::cout << "["
+		  << 1900 + timestamp->tm_year
+		  << ((1 + timestamp->tm_mon <= 9) ? "0" : "")
+		  << 1 + timestamp->tm_mon
+		  << ((1 + timestamp->tm_mday <= 9) ? "0" : "")
+		  << 1 + timestamp->tm_mday << "_"
+		  << ((1 + timestamp->tm_hour <= 9) ? "0" : "")
+		  << 1 + timestamp->tm_hour
+		  << ((1 + timestamp->tm_min <= 9) ? "0" : "")
+		  << 1 + timestamp->tm_min
+		  << ((1 + timestamp->tm_sec <= 9) ? "0" : "")
+		  << 1 + timestamp->tm_sec
+		  << "]"
+		  << " ";
 }
